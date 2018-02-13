2c20b3895ab2a6b070a161cf8b59131f25389806
Android2017C <cnukal7@gmail.com>
2018-02-13 12:52:57 +0530

    activity_main - xml changed

---
+++ app/src/main/java/com/example/sreenureddy/githubdemo/Hello.java
@@ -0,0 +1,16 @@
+package com.example.sreenureddy.githubdemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by SreenuReddy on 2/13/2018.
+ */
+
+public class Hello extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
