45700524468fdbe5113e7a08de3d5fe9e6decac5
Android2017C <cnukal7@gmail.com>
2018-02-13 17:33:42 +0530

    added hello_activity

--- app/src/main/java/com/example/sreenureddy/githubdemo/MainActivity.java
+++ app/src/main/java/com/example/sreenureddy/githubdemo/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView=(Button)findViewById(R.id.Android);
+        textView.setText("Sreenivasulu Reddy C");
 
     }
 }
