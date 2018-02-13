50e8735646c0bc620be9b858e1145b18df92cc69
Android2017C <cnukal7@gmail.com>
2018-02-13 17:22:59 +0530

    added hello_activity

--- app/src/main/java/com/example/sreenureddy/githubdemo/MainActivity.java
+++ app/src/main/java/com/example/sreenureddy/githubdemo/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.sreenureddy.githubdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView=(Button)findViewById(R.id.Android);
+
     }
 }
